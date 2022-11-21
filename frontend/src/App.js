6d32e1484a810ebf2008180ae50d7549b9a8f1eb
Aditya-V-Rathi <rathiaditya.24@gmail.com>
2022-11-21 14:32:52 +0530

    added products and created display page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <a href="/">myshop</a>
-      </header>
-      <main>products list </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
