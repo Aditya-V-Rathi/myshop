f84283b11b19b836bdc46d2a8f7dd9d188246608
Aditya-V-Rathi <rathiaditya.24@gmail.com>
2022-11-21 13:54:03 +0530

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">myshop</a>
+      </header>
+      <main>products list </main>
+    </div>
+  );
+}
+
+export default App;
